8907bc38f66feea72494ec504f52d4fced7b7606
ADAM <adam.niedzialkowski@gmail.com>
2016-10-28 18:04:23 +0200

    Final version

--- lab03/kro-04-02/routes/auth.js
+++ lab03/kro-04-02/routes/auth.js
@@ -2,50 +2,71 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require( 'mongoose' );
 var User = mongoose.model('User');
-
 var bcrypt = require('bcrypt');
+
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
+
+var success_response = {
+	state: 'success',
+	user: {username : undefined}
+};
+var wrong_password_response = {
+	state: 'failure',
+	msg: 'Incorrect user or password'
+};
+var no_users_response = {
+	state: 'failure',
+	msg: 'Currently we have no users in database. Please register first.'
+};
 
 
 	router.route('/login').post(function(req, res){
 		var user_data = req.body;
 		var authenticated = false;
+		var userInDatabase = false;
+
 		User.find(function(err, users){
 			users.forEach(function(user){
 				if(user.username == user_data.username){
-					bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
-    				authenticated = true;
+					userInDatabase = true;
+					bcrypt.compare(user_data.password, user.password, function(err, doesMatch) {
+						if(doesMatch){
+							success_response.user.username = user.username
+							res.send(success_response);
+						} else {
+							res.send(wrong_password_response);
+						}
 					});
 				}
 			});
-		});
 
-		var res_json = {state: 'failure', user: '', message: ''};
-		if(authenticated){
-			return res.send({state: 'success', user: {username : user_data.username}});
-		} else {
-			return res.send({state: 'failure', user: ''});
-		}
+			if(users.length == 0){ // no users in database
+				res.send(no_users_response);
+			} else if (!userInDatabase){
+				res.send(wrong_password_response);
+			}
+		});
 	});
 
 	router.route('/register').post(function(req, res){
 		var user_data = req.body;
 		user = new User();
 		user.username = user_data.username;
-		bcrypt.hash(user_data.password, saltRounds, function(err, hash) {
-			if(!err){
+		bcrypt.hash(user_data.password, saltRounds, function(error, hash) {
+			if(!error){
 				user.password = hash;
-				user.save(function(err, post) {
+				user.save(function(err, hash) {
 						if(err){
 							console.log("Error: " + error);
 						}
-				}););
+				});
+				return true;
 			} else {
-				return res.send(500, err);
+				console.log("Error: " + error);
+				return false;
 			}
 		});
+
 		var res_json = {state: 'failure', user: '', message: ''};
 		return res.send({state: 'success', user: {username : user_data.username}});
 	});
